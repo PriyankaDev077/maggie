58054fe4b503800a6284c764eb56393538e1a5bb
PriyankaDev077 <ikori41@gmail.com>
2024-12-15 17:48:30 +0530

    method

---
+++ main.java
@@ -0,0 +1,12 @@
+class main{
+    public static void main(String[] args)
+    {
+        System.out.println("my maggie recipe");
+        maggie();
+    }
+    public static void maggie(){
+        System.out.println("1.boil water in a pan");
+        System.out.println("2.put maggie");
+        System.out.println("3.and maggie masala");
+    }
+}
